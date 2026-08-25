bb78aea576f1448b0e02158ea350f11b4dfdbd95
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-08-26 00:56:41 +0530

    feat: add global exception handling

---
+++ src/main/java/com/fleetops/notificationservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.fleetops.notificationservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
